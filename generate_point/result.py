7b66a73f356fdffe9cc7cab72aaec04ea9a4e5a5
BrukT <brukteka@gmail.com>
2020-05-14 18:28:09 +0200

    added the error plot script

---
+++ generate_point/result.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+with open("errors.txt") as f:
+lines = f.readlines()
+'''
+
+lines = [250.5451365236493, 206.59592708761403, 197.82137650243587, 195.74338248039163, 189.82899427498484, 186.373895714293, 186.37389571429398]
+plt.plot(lines)
+plt.show()
+
