75c7e599bd3f757969159cadc5c1fa5eba4b9c4e
francesco.barbarulo <barbarulofrancesco@gmail.com>
2020-06-25 17:06:17 +0200

    final clean up

--- hadoop/src/main/java/it/unipi/hadoop/Convergence.java
+++ hadoop/src/main/java/it/unipi/hadoop/Convergence.java
@@ -20,25 +20,25 @@ import java.util.*;
 public class Convergence {
     public static class ConvergenceMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
 
-        List<Point> means;
+        static List<Point> means;
+        final static Text outputKey = new Text();
         final static DoubleWritable outputValue = new DoubleWritable();
-        static double error_accumulator;
+        static double errorAccumulator;
 
         public void setup(Context context) throws IOException {
             Configuration conf = context.getConfiguration();
-            error_accumulator = (double) 0.0;
-            /*
-                prepare the list of means so that using the list we
-                can find the distance a point to the nearest mean.
-             */
+            errorAccumulator = 0.0;
+            means = new ArrayList<>();
 
-            means = new ArrayList<Point>();
+            /*
+                Prepare the list of means so that using the list we
+                can find the distance between a point and the nearest mean.
+             */
 
             URI[] cacheFiles = context.getCacheFiles();
             FileSystem fs = FileSystem.get(conf);
 
             for (URI f: cacheFiles) {
-                System.out.println(f.toString());
                 InputStream is = fs.open(new Path(f));
                 BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
@@ -46,6 +46,7 @@ public class Convergence {
                 while ((line = br.readLine()) != null) {
                     Point mean = new Point(line);
                     means.add(mean);
+                    System.out.print("\n****" + means + "****\n");
                 }
                 br.close();
             }
@@ -60,7 +61,6 @@ public class Convergence {
              */
 
             double minDistance = Double.POSITIVE_INFINITY;
-            Point closestMean = null;
 
             Point p = new Point(value.toString());
 
@@ -71,16 +71,16 @@ public class Convergence {
                 }
             }
 
-            error_accumulator += minDistance;
+            errorAccumulator += minDistance;
         }
 
         public void cleanup(Context context) throws IOException, InterruptedException {
             /*
-            * export the sums of the distances using a comon key for the reducer.
-            * */
-            Text outKey = new Text("common_key");
-            context.write(outKey, new DoubleWritable(error_accumulator));
-
+                Emit the sums of the distances using a common key for the reducer.
+            */
+            outputKey.set("key");
+            outputValue.set(errorAccumulator);
+            context.write(outputKey, outputValue);
         }
     }
 
@@ -89,19 +89,14 @@ public class Convergence {
         static double sum;
         final static DoubleWritable outputValue = new DoubleWritable();
 
-        public void setup(Context context){
-            sum = 0.0;
-        }
-
-        public void reduce(Text key, Iterable<DoubleWritable> values, Context context) {
+        public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
             /*
-                The reducer sums up all the distances that it got from the mappers
+                The single reducer sums up all the distances that it got from the mappers
              */
+            sum = 0.0;
             for (DoubleWritable value: values)
                 sum += value.get();
-        }
 
-        public void cleanup(Context context) throws IOException, InterruptedException {
             outputValue.set(sum);
             context.write(null, outputValue);
         }
